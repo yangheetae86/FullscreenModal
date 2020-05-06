0a8dc7d062c3a2c4cbc6e7fe6a7e479965d88853
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-05-06 18:48:27 +0900

    new

--- FullscreenModal/ContentView.swift
+++ FullscreenModal/ContentView.swift
@@ -1,16 +1,60 @@
-//
-//  ContentView.swift
-//  FullscreenModal
-//
-//  Created by JU HAN LEE on 2020/05/06.
-//  Copyright © 2020 yht. All rights reserved.
+// View { ZStack { NavigationView { VStack { Button { withAnimation { self.isPresented.toggle()
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPresented: Bool = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            NavigationView {
+                VStack {
+                    Button(action: {
+                        withAnimation{
+                        self.isPresented.toggle()//값을 true로 바꾼다 self.isPresented=true
+                        }
+                    }, label: {
+                        Text("Show standard modal")
+                    })
+                }
+                .navigationBarTitle("Standard")
+            }
+            ZStack {
+                HStack {
+                    Spacer()
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                //trigger modal presentation
+                                withAnimation{
+                                self.isPresented.toggle()//값을 true로 바꾼다 self.isPresented=true
+                                }
+                            }, label: {
+                                Text("Dismiss")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            })
+                            Spacer()
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.white)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.isPresented.toggle()
+                                    }
+                                }
+                        }
+                            .padding(.top,
+                                     UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+            }
+            .background(Color.yellow)
+            .edgesIgnoringSafeArea(.all)
+            .offset(x: 0, y: self.isPresented ? 0 :
+                UIApplication.shared.keyWindow?.frame.height ?? 0)
+        }
     }
 }
 
@@ -19,3 +63,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//binding이란 무엇인가를 푼다는의미로 해석, '$'를 앞에두고 @State Bool타입 변수를 넣는다(여기서만?)
+//isPresented 표시됩니다, content 표시되는 내용들
